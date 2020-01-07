d558ad07e9ab6b861e1dcd855660c0c1aa7eeb8b
苏蓉彬 <746996050@qq.com>
2020-01-07 22:06:09 +0800

    vue-infinite-scroll和vue-virtual-scroller使用demo

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { RecycleScroller } from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
+
+Vue.component('RecycleScroller', RecycleScroller)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,86 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div v-show="list.length > 0">
+  <button @click="handleClick">点击</button>
+{{list.length}}
+  <RecycleScroller
+    class="scroller"
+    :items="list"
+    :item-size="32"
+    key-field="id"
+    v-slot="{ item }"
+    v-infinite-scroll="loadMore"
+    nfinite-scroll-disabled="busy"
+    infinite-scroll-distance="10"
+  >
+    <div class="user">
+      {{ item.value }}
+    </div>
+  </RecycleScroller>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import infiniteScroll from 'vue-infinite-scroll'
 
+const defaultList = []
+for (let i = 0; i < 20; i++) {
+  defaultList.push({
+    id: i + 1,
+    value: 'item' + (i + 1)
+  })
+}
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  // props: {
+  //   list: Array,
+  // },
+  directives: { infiniteScroll },
+  data () {
+    return {
+      list: [],
+      busy: false
+    }
+  },
+  methods: {
+    handleClick () {
+      const otherList = []
+      for (let i = 0; i < 20; i++) {
+        otherList.push({
+          id: 'other' + (i + 1),
+          value: 'otheritem' + (i + 1)
+        })
+      }
+      this.list = []
+      setTimeout(() => {
+        this.list = otherList
+      }, 200)
+    },
+    loadMore: function () {
+      console.log('loadMore')
+      this.busy = true
+      const length = this.list.length
+      setTimeout(() => {
+        for (var i = length; i < length + 20; i++) {
+          this.list.push({
+            id: i + 1,
+            value: 'item' + (i + 1)
+          })
+        }
+        this.busy = false
+      }, 1000)
+    }
   }
 }
 </script>
+
+<style scoped>
+.scroller {
+  height: 200px;
+}
+
+.user {
+  height: 32%;
+  padding: 0 12px;
+  display: flex;
+  align-items: center;
+}
+</style>
